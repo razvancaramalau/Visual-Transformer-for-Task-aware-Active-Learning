38b730817d6d4cb7c6ab9678bb713cd532941d8b
razvancaramalau <razvan.caramalau90@gmail.com>
2021-06-07 17:17:15 +0100

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,9 +1,44 @@
+import glob
+import os
 import numpy as np
-from torch.utils.data import DataLoader, Dataset
+import glob
+from PIL import Image
+from torch.utils.data import Dataset
 from config import *
 import torchvision.transforms as T
 from torchvision.datasets import CIFAR100, CIFAR10, FashionMNIST, SVHN
+import albumentations as A
 
+class faceDataset(Dataset):
+    def __init__(self, img_dir):
+        self.img_dir = img_dir
+        k_class = 0
+        self.f_names = []
+        self.labels = []
+        self.ids = []
+        no_dir = os.listdir(self.img_dir)
+        for dir_name in no_dir:
+            # print(self.img_dir + dir_name)
+            self.f_names += glob.glob(self.img_dir + dir_name + "/*.jpg")
+            # print(self.f_names)
+            self.labels  += [k_class for x in range(len(glob.glob(self.img_dir + dir_name  + "/*.jpg")))]
+            k_class += 1
+            # print(self.labels)
+        self.ids = list(range(0, len(self.f_names)))
+
+    def __len__(self):
+        return len(self.f_names)
+
+    def __getitem__(self, index):
+        idx = self.ids[index]
+        transform = T.Compose([T.Resize(128),
+                               T.CenterCrop(128),
+                               T.ToTensor(),
+                               T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        img = transform(Image.open(self.f_names[idx]))
+        target = self.labels[idx]
+        return img, target, idx
+      
 class MyDataset(Dataset):
     def __init__(self, dataset_name, train_flag, transf):
         self.dataset_name = dataset_name
@@ -46,14 +81,16 @@ class MyDataset(Dataset):
 # Data
 def load_dataset(dataset):
     train_transform = T.Compose([
-        T.RandomHorizontalFlip(),
+        T.RandomHorizontalFlip(0.5),
         T.RandomCrop(size=32, padding=4),
         T.ToTensor(),
+        # T.Normalize([0, 0, 0], [1, 1, 1])
         T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]) # T.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) # CIFAR-100
     ])
 
     test_transform = T.Compose([
         T.ToTensor(),
+        # T.Normalize([0, 0, 0], [1, 1, 1])
         T.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]) # T.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)) # CIFAR-100
     ])
 
@@ -64,7 +101,8 @@ def load_dataset(dataset):
         data_test  = CIFAR10('../cifar10', train=False, download=True, transform=test_transform)
         NO_CLASSES = 10
         adden = ADDENDUM
-        no_train = NUM_TRAIN
+        no_train = 50000
+    
     elif dataset == 'cifar10im': 
         data_train = CIFAR10('../cifar10', train=True, download=True, transform=train_transform)
         #data_unlabeled   = CIFAR10('../cifar10', train=True, download=True, transform=test_transform)
@@ -86,14 +124,15 @@ def load_dataset(dataset):
         data_test  = CIFAR10('../cifar10', train=False, download=True, transform=test_transform)
         NO_CLASSES = 10
         adden = ADDENDUM
-        no_train = NUM_TRAIN
+
     elif dataset == 'cifar100':
         data_train = CIFAR100('../cifar100', train=True, download=True, transform=train_transform)
         data_unlabeled = MyDataset(dataset, True, test_transform)
         data_test  = CIFAR100('../cifar100', train=False, download=True, transform=test_transform)
         NO_CLASSES = 100
         adden = 2000
-        no_train = NUM_TRAIN
+        no_train = 50000
+
     elif dataset == 'fashionmnist':
         data_train = FashionMNIST('../fashionMNIST', train=True, download=True, 
                                     transform=T.Compose([T.ToTensor()]))
@@ -102,7 +141,8 @@ def load_dataset(dataset):
                                     transform=T.Compose([T.ToTensor()]))
         NO_CLASSES = 10
         adden = ADDENDUM
-        no_train = NUM_TRAIN
+        no_train = 60000
+
     elif dataset == 'svhn':
         data_train = SVHN('../svhn', split='train', download=True, 
                                     transform=T.Compose([T.ToTensor()]))
@@ -111,5 +151,19 @@ def load_dataset(dataset):
                                     transform=T.Compose([T.ToTensor()]))
         NO_CLASSES = 10
         adden = ADDENDUM
-        no_train = NUM_TRAIN
+        no_train = 73257
+
+    elif dataset == 'rafd':
+        data_train = faceDataset('../cifar10_classif/train/')
+        # if args.which_synthetic == 'matchgan':
+        data_unlabeled = faceDataset('../cifar10_classif/train/')
+        # elif args.which_synthetic == 'stargan':
+        #     data_unlabeled = faceDataset('../../../Justin/rafd/synthetic_by_stargan/')       
+        data_test  = faceDataset('../cifar10_classif/test/')
+        NO_CLASSES = 8
+        adden = 1000
+        no_train = 7200
+
+
+
     return data_train, data_unlabeled, data_test, adden, NO_CLASSES, no_train
