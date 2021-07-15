67433555fdee578b87b644c8aab16eab6dc99352
razvancaramalau <razvan.caramalau90@gmail.com>
2021-07-15 09:22:57 +0100

    Update query_models.py

--- models/query_models.py
+++ models/query_models.py
@@ -5,111 +5,10 @@ import torch.nn.init as init
 import torch.nn.functional as F 
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
-from einops import rearrange
-import numpy as np
-from torch.distributions import Normal
-import math
 
-class GELU(nn.Module):
-    """
-    Paper Section 3.4, last paragraph notice that BERT used the GELU instead of RELU
-    """
-
-    def forward(self, x):
-        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
-
-class Residual(nn.Module):
-    def __init__(self, fn):
-        super().__init__()
-        self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(x, **kwargs) + x
-
-class LayerNormalize(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-class MLP_Block(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.1):
-        super().__init__()
-        self.nn1 = nn.Linear(dim, hidden_dim)
-        torch.nn.init.xavier_uniform_(self.nn1.weight)
-        torch.nn.init.normal_(self.nn1.bias, std = 1e-6)
-        self.af1 = GELU()
-        self.do1 = nn.Dropout(dropout)
-        self.nn2 = nn.Linear(hidden_dim, dim)
-        torch.nn.init.xavier_uniform_(self.nn2.weight)
-        torch.nn.init.normal_(self.nn2.bias, std = 1e-6)
-        self.do2 = nn.Dropout(dropout)
-        
-    def forward(self, x):
-        x = self.nn1(x)
-        x = self.af1(x)
-        x = self.do1(x)
-        x = self.nn2(x)
-        x = self.do2(x)
-        
-        return x
-
-class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dropout = 0.1):
-        super().__init__()
-        self.heads = heads
-        self.scale = dim ** -0.5  # 1/sqrt(dim)
-
-        self.to_qkv = nn.Linear(dim, dim * 3, bias = True) # Wq,Wk,Wv for each vector, thats why *3
-        torch.nn.init.xavier_uniform_(self.to_qkv.weight)
-        torch.nn.init.zeros_(self.to_qkv.bias)
-        
-        self.nn1 = nn.Linear(dim, dim)
-        torch.nn.init.xavier_uniform_(self.nn1.weight)
-        torch.nn.init.zeros_(self.nn1.bias)        
-        self.do1 = nn.Dropout(dropout)
-        
-
-    def forward(self, x, mask = None):
-        b, n, _, h = *x.shape, self.heads
-        qkv = self.to_qkv(x) #gets q = Q = Wq matmul x1, k = Wk mm x2, v = Wv mm x3
-        q, k, v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv = 3, h = h) # split into multi head attentions
-
-        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
-
-        if mask is not None:
-            mask = F.pad(mask.flatten(1), (1, 0), value = True)
-            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
-            mask = mask[:, None, :] * mask[:, :, None]
-            dots.masked_fill_(~mask, float('-inf'))
-            del mask
-
-        attn = dots.softmax(dim=-1) #follow the softmax,q,d,v equation in the paper
-
-        out = torch.einsum('bhij,bhjd->bhid', attn, v) #product of v times whatever inside softmax
-        out = rearrange(out, 'b h n d -> b n (h d)') #concat heads into one matrix, ready for next encoder block
-        out =  self.nn1(out)
-        out = self.do1(out)
-        return out
-
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, mlp_dim, dropout):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                Residual(LayerNormalize(dim, Attention(dim, heads = heads, dropout = dropout))),
-                Residual(LayerNormalize(dim, MLP_Block(dim, mlp_dim, dropout = dropout)))
-            ]))
-    def forward(self, x, mask = None):
-        for attention, mlp in self.layers:
-            att = attention(x, mask = mask) # go to attention
-            x = mlp(att) #go to MLP_Block
-        return x, att
 
 class LossNet(nn.Module):
-    def __init__(self, feature_sizes=[32, 16, 8, 4], num_channels=[64, 128, 256, 512], interm_dim=128):
+    def __init__(self, feature_sizes=[28, 14, 7, 4], num_channels=[64, 128, 256, 512], interm_dim=128):
         super(LossNet, self).__init__()
         
         self.GAP1 = nn.AvgPool2d(feature_sizes[0])
@@ -143,6 +42,7 @@ class LossNet(nn.Module):
 
         out = self.linear(torch.cat((out1, out2, out3, out4), 1))
         return out
+
 
 class GraphConvolution(Module):
     """
@@ -179,36 +79,6 @@ class GraphConvolution(Module):
                + str(self.in_features) + ' -> ' \
                + str(self.out_features) + ')'
 
-
-class GraphConvolution2(Module):
-    """
-    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
-    """
-
-    def __init__(self, in_features, out_features, dropout=0., act=F.relu):
-        super(GraphConvolution2, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.dropout = dropout
-        self.act = act
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        torch.nn.init.xavier_uniform_(self.weight)
-
-    def forward(self, input, adj):
-        input = F.dropout(input, self.dropout, self.training)
-        support = torch.mm(input, self.weight)
-        output = torch.spmm(adj, support)
-        output = self.act(output)
-        return output
-
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
-
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
@@ -221,16 +91,15 @@ class GCN(nn.Module):
 
     def forward(self, x, adj):
         x = F.relu(self.gc1(x, adj))
-        #x = F.relu(self.gc2(x,adj))
         feat = F.dropout(x, self.dropout, training=self.training)
         x = self.gc3(feat, adj)        
         #x = self.linear(x)
         # x = F.softmax(x, dim=1)
         return torch.sigmoid(x), feat, torch.cat((feat,x),1)
 
-class GCN_orig(nn.Module):
+class GCN2(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
-        super(GCN, self).__init__()
+        super(GCN2, self).__init__()
 
         self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc2 = GraphConvolution(nhid, nhid)
@@ -243,9 +112,8 @@ class GCN_orig(nn.Module):
         feat = F.dropout(x, self.dropout, training=self.training)
         x = self.gc3(feat, adj)        
         #x = self.linear(x)
-        x = F.softmax(x, dim=10)
-        return x
-
+        x = F.softmax(x, dim=1)
+        return x, feat, torch.cat((feat,x),1)
 
 class View(nn.Module):
     def __init__(self, size):
@@ -322,244 +190,31 @@ class VAE(nn.Module):
         return latents
 
     def _encode(self, x):
-        return self.encoder(x).cuda()
-
-    def _decode(self, z):
-        return self.decoder(z)
-
-class VTVAE1(nn.Module):
-    """Encoder-Decoder architecture for both WAE-MMD and WAE-GAN."""
-    def __init__(self, z_dim=32, nc=3, f_filt=4, dim = 128, num_tokens = 32, mlp_dim = 256, heads = 8, depth = 6, emb_dropout = 0.1, dropout= 0.1):
-        super(VTVAE1, self).__init__()
-        # self.in_planes = 16
-        self.L = num_tokens
-        self.cT = dim
-        self.z_dim = z_dim
-        self.nc = nc
-        self.f_filt = f_filt
-        self.encoder = nn.Sequential(
-            nn.Conv2d(nc, 128, 4, 2, 1, bias=False),              # B,  128, 32, 32
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.Conv2d(128, 256, 4, 2, 1, bias=False),             # B,  256, 16, 16
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.Conv2d(256, 512, 4, 2, 1, bias=False),             # B,  512,  8,  8
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.Conv2d(512, 1024, self.f_filt, 2, 1, bias=False),            # B, 1024,  4,  4
-            nn.BatchNorm2d(1024),
-            nn.ReLU(True),
-            # View((-1, 1024*2*2)),                                 # B, 1024*4*4
-        )
-        self.fc = nn.Linear(1024*2*2, 1)
-        self.fc_mu = nn.Linear(1024*2*2, z_dim)                            # B, z_dim
-        self.fc_logvar = nn.Linear(1024*2*2, z_dim)                            # B, z_dim
-        self.decoder = nn.Sequential(
-            nn.Linear(z_dim, 1024*4*4),                           # B, 1024*8*8
-            View((-1, 1024, 4, 4)),                               # B, 1024,  8,  8
-            nn.ConvTranspose2d(1024, 512, self.f_filt, 2, 1, bias=False),   # B,  512, 16, 16
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),    # B,  256, 32, 32
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),    # B,  128, 64, 64
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(128, nc, 1),                       # B,   nc, 64, 64
-        )
-
-        self.weight_init()
-        # self.pos_embedding = nn.Parameter(torch.empty(1, (num_tokens + 1), dim))
-        self.pos_embedding = nn.Parameter(torch.empty(1, (num_tokens), dim))
-        torch.nn.init.normal_(self.pos_embedding, std = .02) # initialized based on the paper
-        #Tokenization 
-        self.token_wA = nn.Parameter(torch.empty(32,self.L, 1024),requires_grad = True).cuda() #Tokenization parameters
-        torch.nn.init.xavier_uniform_(self.token_wA)
-        self.token_wV = nn.Parameter(torch.empty(32,1024, self.cT),requires_grad = True).cuda() #Tokenization parameters
-        torch.nn.init.xavier_uniform_(self.token_wV)  
-        #self.patch_conv= nn.Conv2d(64,dim, self.patch_size, stride = self.patch_size) 
-
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, dim)) #initialized based on the paper
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
-
-        self.to_cls_token = nn.Identity()
-
-
-
-    def weight_init(self):
-        for block in self._modules:
-            try:
-                for m in self._modules[block]:
-                    kaiming_init(m)
-            except:
-                kaiming_init(block)
-
-    def forward(self, x, mask=None):
-        z = self._encode(x)
-        z = rearrange(z, 'b c h w -> b (h w) c')
-        # z = rearrange(z, 'b c h w -> b c (h w)')
-
-        wa = rearrange(self.token_wA, 'b h w -> b w h')
-        A= torch.einsum('bij,bjk->bik', z, wa) 
-        A = rearrange(A, 'b h w -> b w h') #Transpose
-        A = A.softmax(dim=-1)
-
-        VV= torch.einsum('bij,bjk->bik', z, self.token_wV)       
-        z = torch.einsum('bij,bjk->bik', A, VV)  
-
-        z += self.pos_embedding
-        z = self.dropout(z)
-        # z, _ = self.transformer(z, mask) #main game
-        z =  z.view(z.size(0), -1)
-
-        x = self.fc(z)
-        return torch.sigmoid(x), #z, mu, logvar
-
-    def reparameterize(self, mu, logvar):
-        stds = (0.5 * logvar).exp()
-        epsilon = torch.randn(*mu.size())
-        if mu.is_cuda:
-            stds, epsilon = stds.cuda(), epsilon.cuda()
-        latents = epsilon * stds + mu
-        return latents
-
-    def _encode(self, x):
-        return self.encoder(x).cuda()
-
-    def _decode(self, z):
-        return self.decoder(z)
-
-class VTVAE2(nn.Module):
-    """Encoder-Decoder architecture for both WAE-MMD and WAE-GAN."""
-    def __init__(self, z_dim=32, nc=3, f_filt=4, dim = 128, num_tokens = 32, mlp_dim = 256, heads = 8, depth = 6, emb_dropout = 0.1, dropout= 0.1):
-        super(VTVAE2, self).__init__()
-        # self.in_planes = 16
-        self.L = num_tokens
-        self.cT = dim
-        self.z_dim = z_dim
-        self.nc = nc
-        self.f_filt = f_filt
-        self.encoder = nn.Sequential(
-            nn.Conv2d(nc, 128, 4, 2, 1, bias=False),              # B,  128, 32, 32
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.Conv2d(128, 256, 4, 2, 1, bias=False),             # B,  256, 16, 16
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.Conv2d(256, 512, 4, 2, 1, bias=False),             # B,  512,  8,  8
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.Conv2d(512, 1024, self.f_filt, 2, 1, bias=False),            # B, 1024,  4,  4
-            nn.BatchNorm2d(1024),
-            nn.ReLU(True),
-            # View((-1, 1024*2*2)),                                 # B, 1024*4*4
-        )
-
-        self.fc_mu = nn.Linear(1024*2*2, z_dim)                            # B, z_dim
-        self.fc_logvar = nn.Linear(1024*2*2, z_dim)                            # B, z_dim
-        self.decoder = nn.Sequential(
-            nn.Linear(z_dim, 1024*4*4),                           # B, 1024*8*8
-            View((-1, 1024, 4, 4)),                               # B, 1024,  8,  8
-            nn.ConvTranspose2d(1024, 512, self.f_filt, 2, 1, bias=False),   # B,  512, 16, 16
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),    # B,  256, 32, 32
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),    # B,  128, 64, 64
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(128, nc, 1),                       # B,   nc, 64, 64
-        )
-        #Tokenization 
-        token_wA = nn.Parameter(torch.empty(z.size()[0],self.L, 1024),requires_grad = True).cuda() #Tokenization parameters
-        torch.nn.init.xavier_uniform_(token_wA)
-        token_wV = nn.Parameter(torch.empty(z.size()[0],1024, self.cT),requires_grad = True).cuda() #Tokenization parameters
-        torch.nn.init.xavier_uniform_(token_wV)  
-
-        self.weight_init()
-        # self.pos_embedding = nn.Parameter(torch.empty(1, (num_tokens + 1), dim))
-        self.pos_embedding = nn.Parameter(torch.empty(1, (num_tokens), dim))
-        torch.nn.init.normal_(self.pos_embedding, std = .02) # initialized based on the paper
-
-
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, dim)) #initialized based on the paper
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, mlp_dim, dropout)
-
-        self.to_cls_token = nn.Identity()
-
-
-    def weight_init(self):
-        for block in self._modules:
-            try:
-                for m in self._modules[block]:
-                    kaiming_init(m)
-            except:
-                kaiming_init(block)
-
-    def forward(self, x, mask=None):
-        z = self._encode(x)
-        z = rearrange(z, 'b c h w -> b (h w) c')
-        # z = rearrange(z, 'b c h w -> b c (h w)')
-
-        wa = rearrange(token_wA, 'b h w -> b w h')
-        A= torch.einsum('bij,bjk->bik', z, wa) 
-        A = rearrange(A, 'b h w -> b w h') #Transpose
-        A = A.softmax(dim=-1)
-
-        VV= torch.einsum('bij,bjk->bik', z, token_wV)       
-        z = torch.einsum('bij,bjk->bik', A, VV)  
-        #print(T.size())
-
-        # cls_tokens = self.cls_token.expand(x.shape[0], -1, -1)
-        # z = torch.cat((cls_tokens, T), dim=1)
-        z += self.pos_embedding
-        z = self.dropout(z)
-        z, att = self.transformer(z, mask) #main game
-        z =  z.view(z.size(0), -1)
-        att =  att.view(z.size(0), -1)
-        # feat = self.to_cls_token(x[:, 0])     
-
-        mu, logvar = self.fc_mu(att), self.fc_logvar(att)
-        z = self.reparameterize(mu, logvar)
-        x_recon = self._decode(z)
-
-        return x_recon, z, mu, logvar
-
-    def reparameterize(self, mu, logvar):
-        stds = (0.5 * logvar).exp()
-        epsilon = torch.randn(*mu.size())
-        if mu.is_cuda:
-            stds, epsilon = stds.cuda(), epsilon.cuda()
-        latents = epsilon * stds + mu
-        return latents
-
-    def _encode(self, x):
         return self.encoder(x)
 
     def _decode(self, z):
         return self.decoder(z)
 
+class DSN(nn.Module):
+	"""Deep Summarization Network"""
+	def __init__(self,in_dim =19,hid_dim=256, num_layers=1):
+		super(DSN, self).__init__()
+		# in_dim = in_dim*in_dim # for semantic segementation
+		in_dim = in_dim 
+		self.lstm = nn.LSTM(in_dim, hid_dim, num_layers=1, bidirectional=True, batch_first=True)
+		self.fc2 = nn.Linear(hid_dim*2,hid_dim*2)
+		self.fc1 = nn.Linear(hid_dim*2,1)
+		
 
-class InnerProductDecoder(nn.Module):
-    """Decoder for using inner product for prediction."""
-
-    def __init__(self, dropout, act=torch.sigmoid):
-        super(InnerProductDecoder, self).__init__()
-        self.dropout = dropout
-        self.act = act
-
-    def forward(self, z):
-        z = F.dropout(z, self.dropout, training=self.training)
-        adj = self.act(torch.mm(z, z.t()))
-        return adj
-
+	def forward(self, x,in_dim=19):
+		
+		# x=torch.reshape(x,(x.shape[0],x.shape[1],in_dim*in_dim)) # for semantic segmentation
+		x = x.float()
+		x,_=self.lstm(x)
+		x= self.fc2(x)
+		x= self.fc1(x)
+		p = torch.sigmoid(x)
+		return p
 
 class Discriminator(nn.Module):
     """Adversary architecture(Discriminator) for WAE-GAN."""
@@ -584,10 +239,50 @@ class Discriminator(nn.Module):
     def forward(self, z):
         return self.net(z)
 
+class Discriminator1(nn.Module):
+    """Adversary architecture(Discriminator) for WAE-GAN."""
+    def __init__(self, z_dim=10):
+        super(Discriminator1, self).__init__()
+        self.z_dim = z_dim
+        self.net = nn.Sequential(
+            nn.Linear(z_dim, 1),
+            # nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
+        self.weight_init()
+
+    def weight_init(self):
+        for block in self._modules:
+            for m in self._modules[block]:
+                kaiming_init(m)
+
+    def forward(self, z):
+        return self.net(z)
+
+class Discriminator2(nn.Module):
+    """Discriminator"""
+    def __init__(self, z_dim=10):
+        super(Discriminator2, self).__init__()
+        self.z_dim = z_dim
+        self.net = nn.Sequential(
+            nn.Linear(z_dim, 512),
+            nn.ReLU(True),
+            nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
+        self.weight_init()
+
+    def weight_init(self):
+        for block in self._modules:
+            for m in self._modules[block]:
+                kaiming_init(m)
+
+    def forward(self, z):
+        return self.net(z)
 
 def kaiming_init(m):
     if isinstance(m, (nn.Linear, nn.Conv2d)):
-        init.kaiming_normal(m.weight)
+        init.kaiming_normal_(m.weight)
         if m.bias is not None:
             m.bias.data.fill_(0)
     elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
